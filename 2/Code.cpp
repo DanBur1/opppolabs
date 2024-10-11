178a9d27a52f48f5c8817cb3abe7af2de6217806
DanBur1 <119559983+DanBur1@users.noreply.github.com>
2024-10-11 20:56:31 +0700

    This one might be right???

--- 2/Code.cpp
+++ 2/Code.cpp
@@ -1,4 +1,5 @@
-﻿#include <fstream>
+﻿/*Copyright [year] <Copyright Owner>*/
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <stdexcept>
@@ -103,7 +104,7 @@ void processCommand(string command) {
       parameters[j][i] = tolower(parameters[j][i]);
     }
   switch (commandMap[parameters[0]]) {
-  case 1: // add
+  case 1:  // add
   {
     if (parameters.size() < 3) {
       cout << "Не указаны параметры" << endl;
@@ -183,7 +184,7 @@ void processCommand(string command) {
     }
     break;
   }
-  case 2: { // rem
+  case 2: {  // rem
     int id;
     try {
       id = stoi(parameters[1]);
@@ -201,7 +202,7 @@ void processCommand(string command) {
     supplies.erase(supplies.begin() + id);
     break;
   }
-  case 3: // print
+  case 3:  // print
   {
     cout << "Вывод:" << endl;
     for (int i = 0; i < supplies.size(); i++) {
@@ -210,7 +211,7 @@ void processCommand(string command) {
     cout << endl;
     break;
   }
-  case 4: // help
+  case 4:  // help
   {
     cout << "Редактор списка канцелярских принадлежностей." << endl
          << "Используются 4 комманды: add (Добавить), rem (Удалить), print "
