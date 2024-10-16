fa053a0e733dc6bd55ca05aefc194b806a28c5da
DanBur1 <119559983+DanBur1@users.noreply.github.com>
2024-10-16 14:19:38 +0700

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,17 +1,17 @@
 /*COPYRIGHT YEAR NO COPYRIGHT*/
 class OfficeSupplies {
- private:
-   float price;
-   string phone_number;
+  private:
+     float price;
+     string phone_number;
 
- public:
-   OfficeSupplies() : price(0), phone_number("") {}
+   public:
+     OfficeSupplies() : price(0), phone_number("") {}
 
-   OfficeSupplies(float p, const string &phone)
-       : price(p), phone_number(phone) {}
+     OfficeSupplies(float p, const string &phone)
+         : price(p), phone_number(phone) {}
 
-   virtual void displayInfo() const {
-    cout << "Цена: " << price << "руб. | Номер владельца: " << phone_number
-          << " |";
-   }
-};
+     virtual void displayInfo() const {
+      cout << "Цена: " << price << "руб. | Номер владельца: " << phone_number
+            << " |";
+     }
+  };
