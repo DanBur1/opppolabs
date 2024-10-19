0ae58943c28f823ddb0e108fd031800e3eef8715
DanBur1 <119559983+DanBur1@users.noreply.github.com>
2024-10-19 11:41:43 +0700

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 ﻿/*COPYRIGHT YEAR NO COPYRIGHT*/
 class MyClass {
 public:
- void Analyze(const std::string &text);
- void Analyze(const char *text, size_t textlen);
+  void Analyze(const std::string &text);
+  void Analyze(const char *text, size_t textlen);
 };
